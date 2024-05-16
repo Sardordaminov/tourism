ea28209ecfaf60344c498404dbe098d4c12fdbe8
Sardordaminov <sd6795838@gmail.com>
2024-05-16 18:20:15 +0500

    salom

---
+++ script.js
@@ -0,0 +1,37 @@
+let valueDisplays = document.querySelectorAll(".statistics_num");
+let interval = 500;
+let totop = document.querySelector(".to-top");
+let header = document.querySelector(".header");
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(() => {
+    startValue += 1;
+    valueDisplay.innerHTML = startValue;
+    if (startValue >= endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    totop.style.display = "flex";
+    header.classList.add = "active";
+  } else {
+    totop.style.display = "none";
+    header.classList.remove
+     = "active";
+  }
+}
+
+totop.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
