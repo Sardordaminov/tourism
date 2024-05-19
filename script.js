0fc45147eac1cd6776b0893175a6a0f52c3bf790
Sardordaminov <sd6795838@gmail.com>
2024-05-19 23:32:32 +0500

    salom

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let valueDisplays = document.querySelectorAll(".statistics_num");
 let interval = 500;
 let totop = document.querySelector(".to-top");
 let header = document.querySelector(".header");
-let open_btn = document.querySelector('.open_btn');
-let menu = document.getElementById('#menu');
 let loadingContainer = document.querySelector('.loading');
 
 valueDisplays.forEach((valueDisplay) => {
@@ -41,8 +39,21 @@ totop.addEventListener("click", function () {
 
 
 
-window.addEventListener('DOMContentLoaded',() => {
+window.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
-  loadingContainer.style.display = "none"
-  },1000)
+    loadingContainer.style.display = "none"
+  }, 2000)
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const openBtn = document.querySelector('.open_btn');
+  const closeBtn = document.querySelector('.close_btn');
+  const headerMenu = document.querySelector('.header-menu');
+
+  openBtn.addEventListener('click', () => {
+    headerMenu.classList.add('active');
+  });
+  closeBtn.addEventListener('click', () => {
+    headerMenu.classList.remove('active');
+  });
+});
